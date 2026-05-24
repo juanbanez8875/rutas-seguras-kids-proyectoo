70fbcd7c645f141a55aeda661d5544f6cbd316a2
Juan Ibañez <juanitx112@gmail.com>
2026-05-24 18:23:14 -0500

    Agregar tarjetas de rutas y boton eliminar

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const formulario = document.getElementById('form-ruta');
+const contenedor = document.getElementById('contenedor-rutas');
 
 formulario.addEventListener('submit', (e) => {
     e.preventDefault(); 
@@ -7,7 +8,25 @@ formulario.addEventListener('submit', (e) => {
     const conductor = document.getElementById('conductor').value;
     const hora = document.getElementById('hora-salida').value;
 
-    console.log({ ruta, conductor, hora });
+    // Creación del elemento visual
+    const nuevaTarjeta = document.createElement('div');
+    nuevaTarjeta.classList.add('tarjeta-ruta'); 
 
+    nuevaTarjeta.innerHTML = `
+        <h3>Ruta: ${ruta}</h3>
+        <p>Conductor: ${conductor}</p>
+        <p>Salida: ${hora}</p>
+        <button class="btn-eliminar">Eliminar Ruta</button>
+        <hr>
+    `;
+
+    // Lógica para eliminar la tarjeta
+    const botonEliminar = nuevaTarjeta.querySelector('.btn-eliminar');
+    botonEliminar.addEventListener('click', () => {
+        nuevaTarjeta.remove(); 
+    });
+
+    // Inserción en la pantalla
+    contenedor.appendChild(nuevaTarjeta);
     formulario.reset();
 });
